68880b81dd8e7ca4e447a6a476468af8cc17c890
Rafael Aybar <rafaelaybar@hotmail.com>
2017-02-28 19:39:53 +0100

    Paso a paso

--- registro.php
+++ registro.php
@@ -3,9 +3,21 @@
     $pass = $_POST['pass'];
     if (isset($usuario) && isset($pass)) {
         if (strlen($pass)>= 8) {
-            //conectamos a la base de datos mysql
-        }
-        else {
+            //ciframos la contraseña
+            $secreto = "Hecho por Rafael Aybar Segura";
+            $passcif = hash_hmac('sha1',$pass,$secreto);
+            //conectamos a la bd (las contraseñas son de prueba, hay que sustituirlas por otras más seguras)
+            $conexion = mysqli_connect("localhost","root","") or
+                        die("conexión errónea");
+            mysqli_select_db ($conexion, "mantis")
+                        or die ("no se puede seleccionar la BD" );
+                          //realizamos la consulta para registrar al usuario
+            $consulta = "INSERT INTO `jugadores` (`nombre`, `deck`, `contrasena`) VALUES ('$usuario', 'NULL', '$passcif');";
+            $insert = mysqli_query($conexion,$consulta) or die("La inserción no se pudo realizar");
+            echo "<h3>Enhorabuena, te has registrado </h3>";
+            mysqli_close($conexion);
+                    }
+      else {
             die("La contraseña ha de ser de 8 caracteres o más");
         }
     }
