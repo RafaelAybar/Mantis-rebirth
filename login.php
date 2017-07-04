ef47d03ad6dbcc6b1402a2740a0279a16e4269ef
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2017-07-04 15:10:11 +0200

    Mejor uso de MySQLI
    Se cambian las funciones de mysqli por unas que son más seguras. Además, ahora se valida de forma más coherente el formulario

--- login.php
+++ login.php
@@ -1,27 +1,32 @@
 <?php
 
-    $usuario = stripslashes(trim($_POST['nombre']));;
-    $pass = stripslashes(trim($_POST['pass']));;
-    
-    if (isset($usuario) && isset($pass)) {
+    if (isset($_POST['nombre']) && isset($_POST['pass'])) {
+        $usuario = stripslashes(trim($_POST['nombre']));
+        $pass = stripslashes(trim($_POST['pass']));
         if (strlen($pass)>= 8) {
-                $conexion = mysqli_connect("localhost","root","") or
-                        die("conexión errónea");
-            mysqli_select_db ($conexion, "mantis")
-                        or die ("no se puede seleccionar la BD" );
-            //obtenemos el usuario y su contraseña del usuario registrado
-            $consulta = "SELECT contrasena FROM jugadores WHERE (nombre='$usuario');";
-            $comprobacion = mysqli_query($conexion,$consulta) or die("Autentificación fallida");
-            $columnas = $comprobacion->fetch_array(MYSQLI_ASSOC);
-          if (password_verify($pass, $columnas['contrasena'])) {
-              echo "Enhorabuena, te has logueado correctamente <br>";
-          }
-                
-            
-            mysqli_close($conexion);
-                    }
-    }
-    else {
-        die("Mira, te comento, debes introducir los datos, ¿VALE?");
+                //He seguido la siguiente documentación: https://www.w3schools.com/php/php_mysql_prepared_statements.asp
+                //Definimos los parámetros de mysqli, primero estableciendo los parámetros de conexión:
+                $nombreserver = "localhost";
+                $usuario = "root";
+                $contra = "";
+                $bd = "mantis";
+                $conexion = mysqli_connect($nombreserver, $usuario, $contra, $db);
+            //comprobamos que podemos establecer conexión
+           if ($conexion -> connect_error){
+            die("No se pudo conectar ".$conexion->connect_error);
+           }
+           else {
+               //Preparamos la consulta
+               $consultaprep = $conexion -> mysqli_prepare("SELECT EXISTS (SELECT contrasena FROM jugadores WHERE (nombre = ?))");
+               //Ligamos los parámetros
+               $consultaprep = $conexion -> mysqli_bind_param("s", $usuario);
+               //Ejecutamos la consulta
+               $consultaprep ->execute();
+
+           }
+        }
+        else {
+            die("Mira, te comento, debes introducir los datos, ¿VALE?");
+        }
     }
 ?>
