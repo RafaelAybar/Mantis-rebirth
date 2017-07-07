6ed063b26a35c2e59ad20c512d82a068bfe0ca2b
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2017-07-07 13:26:32 +0200

    El último else está ahora bien puesto

--- login.php
+++ login.php
@@ -5,8 +5,8 @@
         $pass = stripslashes(trim($_POST['pass']));
 
         //ciframos la contraseña
-        $coste = ['coste'=>18];
-        $passhash = password_hash($pass, PASSWORD_DEFAULT,$coste);
+        //$coste = ['coste'=>18];
+        //$passhash = password_hash($pass, PASSWORD_DEFAULT,$coste);
         if (strlen($pass)>= 8) {
                 //He seguido la siguiente documentación: https://www.w3schools.com/php/php_mysql_prepared_statements.asp
                 //Definimos los parámetros de mysqli, primero estableciendo los parámetros de conexión:
@@ -21,18 +21,28 @@
            }
            else {
                //Preparamos la consulta
-               $consultaprep = $conexion -> prepare("SELECT contrasena FROM jugadores WHERE (nombre = ?) AND (contrasena = ?))");
+               $consultaprep = $conexion -> prepare("SELECT contrasena FROM jugadores WHERE (nombre = ?) AND (contrasena = ?)");
                //Ligamos los parámetros
-               $consultaprep -> bind_param("ss", $usuario, $passhash);
+               $consultaprep -> bind_param("ss", $usuario, $pass);
                //Ejecutamos la consulta
                $consultaprep ->execute();
-               echo "$consultaprep";
+               $consulta = $consultaprep -> fetch();
+               
+               if(password_verify($pass,$consulta)){
+                    echo "Enhorabuana, te has logueado";
+               }
+               else {
+                   die("La contraseña o el nombre no coinciden");
+               }
                $consultaprep -> close();
                $conexion -> close();
            }
         }
         else {
-            die("Mira, te comento, debes introducir los datos, ¿VALE?");
+            die("La contraseña no cumple los requisitos");
         }
     }
+    else {
+            die("Mira, te comento, debes introducir los datos, ¿VALE?");
+        }
 ?>
