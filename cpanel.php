2410e589e319ca2fe166392978f6b70d0553617d
Rafael Aybar <rafaelaybar@hotmail.com>
2017-07-09 11:41:02 +0200

    fecha sesion login y fecha sesion cpanel terminadas

--- cpanel.php
+++ cpanel.php
@@ -1,2 +1,24 @@
 <?php
+session_start();
+echo "Bienvenido ".$_SESSION['nick']." , has iniciado sesión en la fecha ".$_SESSION['instante']."</br>";
+$fechanueva = date("Y-m-d H:i:s");
+echo "</br>";
+//Comprobamos que es una cadena
+if (is_string($fechanueva) && is_string($_SESSION['instante'])) {
+    //Pasamos la cadena a valores de feccha
+    $fecha1 = date_create($_SESSION['instante']);
+    $fecha2 = date_create($fechanueva);
+    //Calculamos la diferencia 
+    $diferencia = date_diff($fecha1,$fecha2);
+    //Especificamos que si la difencia es mayor de 4 horas la sesión caduque
+    if ($diferencia['h'] < 4) {
+        echo "Se ha agotado el tiempo de sesión";
+        session_unset();
+        session_destroy();
+    }
+
+}
+else {
+    die("No se pudo realizar");
+}
 ?>
--- login.php
+++ login.php
@@ -4,7 +4,7 @@
         $nickjugador = stripslashes(trim($_POST['nombre']));
         $pass = stripslashes(trim($_POST['pass']));
         //Definimos el coste hash
-       $coste = ['coste'=>18];
+        $coste = ['coste'=>18];
         if (strlen($pass)>= 8) {
                 //He seguido la siguiente documentación: https://www.w3schools.com/php/php_mysql_prepared_statements.asp
                 //Definimos los parámetros de mysqli, primero estableciendo los parámetros de conexión:
@@ -14,14 +14,16 @@
                 $bd = "mantis";
                 $conexion = mysqli_connect($nombreserver, $usuario, $contra, $bd);
             //comprobamos que podemos establecer conexión
+           
            if ($conexion -> connect_error){
-            die("No se pudo conectar ".$conexion->connect_error);
+                die("No se pudo conectar ".$conexion->connect_error);
            }
            else {
                //Definimos el un objeto, que se llama usuario y sus propiedades
-               $jugador = (object)[
-                   'nombre'=> $nickjugador,
-               ];
+                $jugador = (object)[
+                    'nombre'=> $nickjugador,
+                ];
+
                //Preparamos la consulta
                $consultaprep = $conexion -> prepare("SELECT contrasena FROM jugadores WHERE (nombre = ?)");
                //Ligamos los parámetros
@@ -31,13 +33,16 @@
                //Ligamos el resultado a una variable, un string
                $consultaprep -> bind_result($resultado);
                $consultaprep -> fetch();
+               
                //Comprobamos que coinciden las contraseñas
-                if (password_verify($pass,$resultado)) {
+               if (password_verify($pass,$resultado)) {
                     session_start();
-                    echo "Enhorabuena, te has logueado, puedes acceder a <a href='cpanel.php'>Panel de control</a>";
+                    $_SESSION['nick'] = $nickjugador;
+                    $_SESSION['instante'] = date("Y-m-d H:i:s");
+                    echo "Enhorabuena ".$_SESSION['nick'].", te has logueado, puedes acceder a <a href='cpanel.php'>Panel de control</a>";
                 }
                 else {
-                    die("El nombre o la contraseña no funciona");
+                    die("El nombre o la contraseña no coinciden");
                 }
                 $consultaprep -> close();
                 $conexion ->close();
