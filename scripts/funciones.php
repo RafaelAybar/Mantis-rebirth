d13b8b966d8d80f9b392201683fde2b975a622c2
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2019-03-24 19:40:08 +0100

    Las contraseñas están bien validadas

--- scripts/funciones.php
+++ scripts/funciones.php
@@ -9,18 +9,19 @@ function validaNombre(){
     if (isset($_POST['nombre'])){
         //Comprobamos que se cumple el patrón y no llegan valores nulos
         if (preg_match('/^[a-z0-9_\-]+$/i',$_POST['nombre']) === 1 && strtolower($_POST['nombre']) != "null" &&
-            strlen($_POST['nombre']) <= 4){
+            strlen($_POST['nombre']) >= 4){
             $nombre = (string) $_POST['nombre'];
         }
         else{
-            throw new Exception("Nombre no válido, <a href='../registro.html'> Volver al registro</a>");
+            throw new Exception("Nombre no válido");
         }
         return $nombre;
     }
     else{
-        throw new Exception("Debes introducir el nombre, <a href='../registro.html'> Volver al registro</a>");
+        throw new Exception("Debes introducir el nombre");
     }
     }
+
 
 /**
  * @return bool|string
@@ -28,18 +29,36 @@ function validaNombre(){
  */
 function validaYCifraPass(){
     if (isset($_POST['pass']) && isset($_POST['pass2'])){
-            if (strtolower($_POST['pass']) != "null" && $_POST['pass']) {
-                $contrasena = (string)$_POST['pass'];
-                $contrasena2 = (string)$_POST['pass'];
-                if ($contrasena === $contrasena2 && strlen($_POST['pass']) <= 8) {
+            if (preg_match('/^[a-z0-9_\-]+$/i',$_POST['pass']) === 1) {
+                $contrasena = (string) $_POST['pass'];
+                $contrasena2 = (string) $_POST['pass2'];
+                if ($contrasena === $contrasena2 && strlen($contrasena) >= 8) {
                     //Validada la contraseña del usuario, llega el momento de cifrarla
                     $coste = ['coste' => 18]; //Aumentamos el coste del algoritmo
                     $passhash = password_hash($contrasena, PASSWORD_DEFAULT, $coste);
                     return $passhash;
                 }
+                else{
+                    throw new Exception("Las contraseñas no coinciden, y deben ser de 8 o más caracteres");
+                }
+            }
+            else {
+                throw new Exception("La contraseña no es válida");
+                
             }
         }
-    else{
-        throw new Exception("Revisa que las contraseñas sean mayores o iguales de 8 caracteres y qye sean iguales, <a href='../registro.html'> Volver al registro</a>");
+    else {
+        throw new Exception("Debes introducir la contraseña");
     }
+    
+}
+
+function conectaEInsertaEnBD(){
+    //https://phpdelusions.net/pdo
+    $host = '127.0.0.1';
+    $bd = 'mantis';
+    $user = 'ras';
+    $pass = '';
+    $codificacion = 'https://phpdelusions.net/pdo';
+    $dsn = "mysql_host:";
 }
--- scripts/registro.php
+++ scripts/registro.php
@@ -3,10 +3,8 @@ include 'funciones.php';
 try {
     $nombre = validaNombre();
     $passcifrada = validaYCifraPass();
+    var_dump($passcifrada); //Indica que tiene un valor nulo
 } catch (Exception $e){
     echo $e->getMessage();
-    die("<a href='../registro.html'>Vuelve al registro</a>");
+    die("<a href='../registro.html'> Vuelve al registro</a>");
 }
-echo "$nombre <br>";
-echo "$passcifrada <br>";
-echo 2;
