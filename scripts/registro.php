f65b43b706c320bcda46a73a902cfbb99a2030b3
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2019-03-24 15:39:38 +0100

    Ahora las pass se validan correctamente

--- scripts/registro.php
+++ scripts/registro.php
@@ -1,4 +1,12 @@
 <?php
 include 'funciones.php';
-$nombre=validaNombre();
-$passcifrada=validaYCifraPass();
+try {
+    $nombre = validaNombre();
+    $passcifrada = validaYCifraPass();
+} catch (Exception $e){
+    echo $e->getMessage();
+    die("<a href='../registro.html'>Vuelve al registro</a>");
+}
+echo "$nombre <br>";
+echo "$passcifrada <br>";
+echo 2;
