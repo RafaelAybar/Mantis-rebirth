e44b7ce80bc4d91eea5936da98752d4e08241f30
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2019-04-18 04:25:02 +0200

    Añadido CSS y terminado el registro del usuario

--- scripts/registro.php
+++ scripts/registro.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../bulma.css">
+    <title>Formulario de registro</title>
+</head>
+<body>
+</body>
+</html>
 <?php
 include 'funciones.php';
 try {
