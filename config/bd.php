4697b0b9f2049eff9f4e21d1f02fdd713ac69209
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2019-04-20 03:02:25 +0200

    Terminada conexión a la base de datos

---
+++ config/bd.php
@@ -0,0 +1,10 @@
+<?php
+// Parámetros de conexión de la BD
+return [
+    "driver" => "mysql",
+    "host" => "localhost",
+    "user" => "root",
+    "pass" => "Rafa-1995",
+    "database" => "mantis",
+    "charset" => "utf8mb4",
+    ];
---
+++ model/Conectar.php
@@ -0,0 +1,41 @@
+<?php
+
+class Conectar
+{
+    private $driver;
+    private $host, $user, $pass, $database, $charset;
+
+
+    /**
+     * Conectar constructor.
+     */
+    public function __construct()
+    {
+        $db_cfg = require_once 'config/bd.php';
+
+        $this->driver = $db_cfg['driver'];
+        $this->host = $db_cfg['host'];
+        $this->user = $db_cfg['user'];
+        $this->pass = $db_cfg["pass"];
+        $this->database = $db_cfg["database"];
+        $this->charset = $db_cfg["charset"];
+    }
+
+    /**
+     * @param $db_cfg
+     */
+    public function conexion()
+    {
+        $dsn = "$this->driver:host=$this->host;dbname=$this->database;charset=$this->charset";
+        $opcionesPDO = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ];
+        try {
+            $pdo = new PDO($dsn, $this->user, $this->pass, $opcionesPDO);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage());
+        }
+    }
+}
